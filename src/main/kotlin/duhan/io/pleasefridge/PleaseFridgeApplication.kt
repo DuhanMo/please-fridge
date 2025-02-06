2d02e2c652c5445310d5dd528ded94b4b372e28f
Duhan Mo <duhandv@gmail.com>
2025-02-06 23:40:22 +0900

    first commit

---
+++ src/main/kotlin/duhan/io/pleasefridge/PleaseFridgeApplication.kt
@@ -0,0 +1,11 @@
+package duhan.io.pleasefridge
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PleaseFridgeApplication
+
+fun main(args: Array<String>) {
+    runApplication<PleaseFridgeApplication>(*args)
+}
